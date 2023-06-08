7d2038249173dc1d0528e6d831ece12b2305c727
Giuseppe Pinto <egpinto@minsait.com>
2023-06-08 15:54:08 +0200

    add both projects

---
+++ angular-host/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ react-mfe/webpack.config.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devMode = true;
+const BUILD_PATH = '/dist/micro-frontend';
+
+module.exports = {
+  entry: './src/App.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  output: {
+    path: __dirname + BUILD_PATH,
+    filename: 'micro-frontend.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
+};
